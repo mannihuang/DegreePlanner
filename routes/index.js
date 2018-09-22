e6ecd883af5bc7973528aac5272f632c125b6c59
nhat286 <thiennhatnguyenminh@gmail.com>
2018-09-22 12:29:54 +1000

    add user template and add course logic

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var app = express();
+//var courseRoute = require('routes/course.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-var server = app.listen(8000, function() {});
 module.exports = router;
 
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+var courses = [];
+var db_courses = ['COMP1511', 'COMP1521', 'COMP1531', 'COMP2521', 'COMP2511', 'COMP2041']
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('planner', {chosen_courses: courses, av_courses: db_courses});
+});
+
+router.post('/addCourse', function(req, res, next) {
+  var added = false;
+  var choice = req.body.courseCode;
+  for (var i = 0; i < courses.length; i++) {
+    if (courses[i] === choice) {
+      added = true;
+      break;
+    }
+  }
+  if (added === false) {
+    courses.push(req.body.courseCode);
+  }
+  res.redirect('/users');
 });
 
 module.exports = router;
